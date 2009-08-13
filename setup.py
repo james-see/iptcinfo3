364d849ecd8e310455ad3cc2e012c30670480eeb
gthomas <gthomas@hackworth.guba>
2009-08-13 22:26:55 +0200

    setup.py: documentation corrected (thanks to Joh Heasly)

--- setup.py
+++ setup.py
@@ -21,86 +21,88 @@ Python programs, export it to XML, or even export SQL statements ready
 to be fed into a database.
 
 1.9.2-rc8:
-  charset recognition loosened (failed with some image out of 
-  Adobe Lightroom).
+    charset recognition loosened (failed with some image out of
+    Adobe Lightroom).
 
 1.9.2-rc7: NOT READY
-  IPTCInfo now accepts 'inp_charset' keyword for setting input charset.
+    IPTCInfo now accepts 'inp_charset' keyword for setting input charset.
 
 1.9.2-rc6: just PyLint-ed out some errors.
 
 1.9.2-rc5: Amos Latteier sent me a patch which releases the requirement of the
-  file objects to be file objects (he uses this on jpeg files stored in
-  databases as strings).
-    It modifies the module in order to look for a read method on the file
-    object. If one exists it assumes the argument is a file object, otherwise it
-    assumes it's a filename.
+    file objects to be file objects (he uses this on jpeg files stored in
+    databases as strings).
+        It modifies the module in order to look for a read method on the file
+        object. If one exists it assumes the argument is a file object, otherwise it
+        assumes it's a filename.
 
 1.9.2-rc4: on Windows systems, tmpfile may not work correctly - now I use
-  cStringIO on file save (to save the file without truncating it on Exception).
+    cStringIO on file save (to save the file without truncating it on Exception).
 
 1.9.2-rc3: some little bug fixes, some safety enhancements (now iptcinfo.py
-  will overwrite the original image file (info.save()) only if everything goes
-  fine (so if an exception is thrown at writing, it won't cut your original
-  file).
+    will overwrite the original image file (info.save()) only if everything goes
+    fine (so if an exception is thrown at writing, it won't cut your original
+    file).
 
-  This is a pre-release version: needs some testing, and has an unfound bug
-  (yet): some pictures can be enhanced with iptc data, and iptcinfo.py is able
-  to read them, but some other iptc data readers will spit on it.
+    This is a pre-release version: needs some testing, and has an unfound bug
+    (yet): some pictures can be enhanced with iptc data, and iptcinfo.py is able
+    to read them, but some other iptc data readers will spit on it.
 
 1.9.1: a first release with some little encoding support
 
-  The class IPTCInfo now has an inp_charset and an out_charset attribute - the
-  first is the read image's charset (defaults to the system default charset),
-  the second is the charset the writer will use (defaults to inp_charset).
+    The class IPTCInfo now has an inp_charset and an out_charset attribute - the
+    first is the read image's charset (defaults to the system default charset),
+    the second is the charset the writer will use (defaults to inp_charset).
 
-  Reader will find the charset included in IPTC data (if any, defaults to the
-  system's default charset), and use it to read to unicode strings. Writer will
-  write using IPTCinfo.out_charset (if it is not set, will not write charset
-  IPTC record).
+    Reader will find the charset included in IPTC data (if any, defaults to the
+    system's default charset), and use it to read to unicode strings. Writer will
+    write using IPTCinfo.out_charset (if it is not set, will not write charset
+    IPTC record).
 
-  With this, it is possible to read and write i18n strings correctly.
+    With this, it is possible to read and write i18n strings correctly.
 
-  I haven't tested this functionality thoroughly, and that little test was only
-  on my WinXP box only, with the only other IPTC reader: IrfanView.
+    I haven't tested this functionality thoroughly, and that little test was only
+    on my WinXP box only, with the only other IPTC reader: IrfanView.
 
 
 SYNOPSIS
 
-  from iptcinfo import IPTCInfo
-  import sys
+    from iptcinfo import IPTCInfo
+    import sys
 
-  fn = (len(sys.argv) > 1 and [sys.argv[1]] or ['test.jpg'])[0]
-  fn2 = (len(sys.argv) > 2 and [sys.argv[2]] or ['test_out.jpg'])[0]
+    fn = (len(sys.argv) > 1 and [sys.argv[1]] or ['test.jpg'])[0]
+    fn2 = (len(sys.argv) > 2 and [sys.argv[2]] or ['test_out.jpg'])[0]
 
-  # Create new info object
-  info = IPTCInfo(fn)
+    # Create new info object
+    info = IPTCInfo(fn)
 
-  # Check if file had IPTC data
-  if len(info.data) < 4: raise Exception(info.error)
+    # Check if file had IPTC data
+    if len(info.data) < 4: raise Exception(info.error)
 
-  # Print list of keywords, supplemental categories, contacts
-  print info.keywords
-  print info.supplementalCategories
-  print info.contacts
+    # Print list of keywords, supplemental categories, contacts
+    print info.keywords
+    print info.supplementalCategories
+    print info.contacts
 
-  # Get specific attributes...
-  caption = info.data['caption/abstract']
+    # Get specific attributes...
+    caption = info.data['caption/abstract']
 
-  # Create object for file that may or may not have IPTC data.
-  info = IPTCInfo(fn)
+    # Create object for file that may does have IPTC data.
+    # info = IPTCInfo(fn)
+    # for files without IPTC data, use
+    info = IPTCInfo(fn, force=True)
 
-  # Add/change an attribute
-  info.data['caption/abstract'] = 'Witty caption here'
-  info.data['supplemental category'] = ['portrait']
+    # Add/change an attribute
+    info.data['caption/abstract'] = 'Witty caption here'
+    info.data['supplemental category'] = ['portrait']
 
-  # Save new info to file
-  ##### See disclaimer in 'SAVING FILES' section #####
-  info.save()
-  info.saveAs(fn2)
+    # Save new info to file
+    ##### See disclaimer in 'SAVING FILES' section #####
+    info.save()
+    info.saveAs(fn2)
 
-  #re-read IPTC info
-  print IPTCInfo(fn2)
+    #re-read IPTC info
+    print IPTCInfo(fn2)
 """
 
 classifiers = """\
@@ -118,41 +120,41 @@ from distutils.command.sdist import sdist as _sdist
 import sys
 
 if sys.version_info < (2, 3):
-  _setup = setup
-  def setup(**kwargs):
-    if kwargs.has_key("classifiers"):
-      del kwargs["classifiers"]
-      _setup(**kwargs)
+    _setup = setup
+    def setup(**kwargs):
+        if kwargs.has_key("classifiers"):
+            del kwargs["classifiers"]
+            _setup(**kwargs)
 
 class sdist(_sdist, object):
-  def run(self):
-    import os
-    res = _sdist.run(self)
-    print self.get_archive_files()
-    for fn in self.get_archive_files():
-      os.system('scp -p %s gtmainbox:/var/www/html/python/' % fn)
-    return res
-  
+    def run(self):
+        import os
+        res = _sdist.run(self)
+        print self.get_archive_files()
+        for fn in self.get_archive_files():
+            os.system('scp -p %s gtmainbox:/var/www/html/python/' % fn)
+        return res
+
 
 doclines = __doc__.split("\n")
 
 version = '1.9.2-rc8'
 #zipext = (sys.platform.startswith('Win') and ['zip'] or ['tar.gz'])[0]
 setup(#cmdclass={'sdist': sdist},
-      name='IPTCInfo',
-      version=version,
-      url='http://bitbucket.org/gthomas/iptcinfo/',
-      download_url='http://gthomas.homelinux.org/hg/hgwebdir.cgi/repos/iptcinfo/archive/tip.tar.bz2',
-      #url='http://freehg.org/u/gthomas/iptcinfo/',
-      #download_url='http://fo-%s.%s' % (version, zipext),
-      author=u'Tamas Gulacsi',
-      author_email='gthomas@fw.hu',
-      maintainer=u'Tamas Gulacsi',
-      maintainer_email='gthomas@fw.hu',
-      license = 'http://www.opensource.org/licenses/gpl-license.php',
-      platforms = ['any'],
-      description = doclines[0],
-      classifiers = filter(None, classifiers.split('\n')),
-      long_description = '\n'.join(doclines[2:]),
-      py_modules=['iptcinfo'],
-      )
+    name='IPTCInfo',
+    version=version,
+    url='http://bitbucket.org/gthomas/iptcinfo/',
+    download_url='http://gthomas.homelinux.org/hg/hgwebdir.cgi/repos/iptcinfo/archive/tip.tar.bz2',
+    #url='http://freehg.org/u/gthomas/iptcinfo/',
+    #download_url='http://fo-%s.%s' % (version, zipext),
+    author=u'Tamas Gulacsi',
+    author_email='gthomas@fw.hu',
+    maintainer=u'Tamas Gulacsi',
+    maintainer_email='gthomas@fw.hu',
+    license = 'http://www.opensource.org/licenses/gpl-license.php',
+    platforms = ['any'],
+    description = doclines[0],
+    classifiers = filter(None, classifiers.split('\n')),
+    long_description = '\n'.join(doclines[2:]),
+    py_modules=['iptcinfo'],
+    )
