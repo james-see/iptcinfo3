dd360c25e2b2df5cbb4d2e1b261bf8ef2f84c28b
Tamás Gulácsi <gt-dev@gthomas.homelinux.org>
2011-03-21 20:03:32 +0100

    upl.sh fix

--- upl.sh
+++ upl.sh
@@ -2,5 +2,5 @@
 hg push && hg push bitbucket && python setup.py -v register && {
   FILE=$(ls -t dist/IPTCInfo-*.tar.gz| head -n 1)
   scp -p $FILE gthomas.homelinux.org:/var/www/html/python/
-  wput $FILE ftp://gthomas@ftp.fw.hu/gthomas/python/
+  curl -T "$FILE" -u "$PASSW" ftp://ftp.fw.hu/gthomas/python/
 }
