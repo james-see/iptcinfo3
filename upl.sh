3f442e2414aeca1a4586d32b74893bdb469a0856
Tamás Gulácsi <gt-dev@gthomas.homelinux.org>
2011-07-25 21:51:24 +0200

    fix upl.sh to use "setup.py sdist upload"

--- upl.sh
+++ upl.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-hg push && hg push bitbucket && python setup.py -v register && {
-  FILE=$(ls -t dist/IPTCInfo-*.tar.gz| head -n 1)
-  scp -p $FILE gthomas@gthomas.homelinux.org:html/python/
-  curl -T "$FILE" -u "$PASSW" ftp://ftp.fw.hu/gthomas/python/
-}
+hg push && hg push bitbucket && python setup.py -v register \
+  && python setup.py sdist -d dist upload && {
+    FILE=$(ls -t dist/IPTCInfo-*.tar.gz| head -n 1)
+    scp -p $FILE gthomas@gthomas.homelinux.org:html/python/
+    curl -T "$FILE" ftp://gthomas@ftp.fw.hu/gthomas/python/
+  }
