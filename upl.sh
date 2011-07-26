a865695ef3067529d6a20d49342c959a3c5e120d
Tamás Gulácsi <gt-dev@gthomas.homelinux.org>
2011-07-26 07:01:31 +0200

    create tag according to version, if not exists

--- upl.sh
+++ upl.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
-hg push && hg push bitbucket && python setup.py -v register \
-  && python setup.py sdist -d dist upload && {
-    FILE=$(ls -t dist/IPTCInfo-*.tar.gz| head -n 1)
-    scp -p $FILE gthomas@gthomas.homelinux.org:html/python/
-    curl -T "$FILE" ftp://gthomas@ftp.fw.hu/gthomas/python/
-  }
+VERSION=$(sed -ne '/^__version__/ { s/^[^0-9]*//;s/. *$//p }' iptcinfo.py)
+echo VERSION=$VERSION
+hg tags | grep -q $VERSION || {
+  echo "tagging $VERSION"
+  hg tag "iptcinfo-$VERSION" || exit 2
+}
+echo 'hg push...'  && hg push \
+&& echo 'hg push bitbucket...' && hg push bitbucket \
+&& echo 'python setup.py register...' && python setup.py -v register \
+&& echo 'python setup.py sdist upload...' \
+&& python setup.py sdist -d dist upload && {
+  FILE=dist/IPTCInfo-${VERSION}.tar.gz
+  echo "scp $FILE gho:html/python/..."
+  scp -p $FILE gthomas@gthomas.homelinux.org:html/python/
+  curl -T "$FILE" ftp://gthomas@ftp.fw.hu/gthomas/python/
+}
