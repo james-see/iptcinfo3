897235bce5a071d9c686fe9dbb9cb909235278be
crccheck <c@crccheck.com>
2018-01-14 17:02:33 -0600

    change to use getitem/setitem

--- iptcinfo3.py
+++ iptcinfo3.py
@@ -598,12 +598,11 @@ class IPTCInfo:
     # Attributes for clients
     #######################################################################
 
-    def getData(self):
-        return self._data
+    def __getitem__(self, key):
+        return self._data[key]
 
-    def setData(self, _):
-        raise Exception('You cannot overwrite the data, only its elements!')
-    data = property(getData, setData)
+    def __setitem__(self, key, value):
+        self._data[key] = value
 
     def __str__(self):
         return ('charset: %s\n%s' % (self.inp_charset,
